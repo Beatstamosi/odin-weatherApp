9d8a15093f4a9a1aa2886b67990797eb3c1f705c
Beatstamosi <moritz.lued@gmail.com>
2025-02-08 12:27:03 -0500

    Decrease font & icon size

--- src/index.js
+++ src/index.js
@@ -2,3 +2,28 @@ import "./styles.css";
 import addButtonListenerGetWeather from "./buttonlistener";
 
 addButtonListenerGetWeather();
+
+
+
+// eventlistener "enter" to searchbar
+    // call fetchAPI
+// eventlistener "click" to enter-button
+    // call fetchAPI
+
+// render info
+    // content today
+        // content-left today
+        // details today
+        // --> get fields and set content
+    // weekly forecast, for each element create proper HTML
+        // transform date into weekday
+        // transform date into "Month, day"
+
+
+// button celsius and fahrenheit
+    // call fetchAPI with celsius or Fahrenheit
+    // render info
+
+
+
+// SET UP DETAILS TODAY WITH CORRECT IMAGES
